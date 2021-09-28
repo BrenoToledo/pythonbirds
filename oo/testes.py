1cdac2793a5eb6a4eb813dd0507b9a5946ea618c
Breno Toledo <breh_toledo@hotmail.com>
2021-09-28 11:49:31 -0300

    testes.py - Explicação sobre os tipos de testes

---
+++ oo/testes.py
@@ -0,0 +1,15 @@
+"""
+Existem alguns tipos e niveis de testes, um deles é o
+
+Unit - que faz o teste unitário,
+testando então cada classe por vez sem interligações externas.
+
+geralmente neste tipo de teste, é necessário criar classes de
+apoio para simularmos o funcionamento, essas classes geralmente tem o
+nome de moke
+
+Depois que passar todos os testes unitarios, devemos fazer os testes de
+integração.
+
+Integração -
+"""
